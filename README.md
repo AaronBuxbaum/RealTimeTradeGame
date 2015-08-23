4d36f892ea74d433b090a6f1ea5e68b9b9b10634
Aaron Buxbaum <abuxbaum@factset.com>
2015-08-23 14:13:08 -0400

    change the build process

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ To install:
 
 1. Clone git repo
 2. Run `npm install`. You may need to install `npm` from npmjs.com.
-3. Build with `gulp`. You may need to install `gulp` by running `npm install --global gulp`.
+3. Build with `gulp build`. You may need to install `gulp` by running `npm install --global gulp`.
+4. Run the server with `gulp`.
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,42 +1,83 @@
 /* Dependencies */
 var gulp = require('gulp');
 var path = require('path');
+var tsd = require('gulp-tsd');
 var less = require('gulp-less');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var install = require('gulp-install');
-var server = require('gulp-express');
+var express = require('gulp-express');
 
 
 /* Global variables */
-var SRC = 'src/';
-var BOWER = 'bower_components/';
-var BUILD = 'build/';
+var SRC = '../src/';
+var NPM = '../node_modules/';
+var BOWER = '../bower_components/';
+var BUILD = '../build/';
+var TYPINGS = '../typings/';
 var MODULES = ['ticker', 'database', 'stocks'];
 
 
-/* Non-default tasks */
+/* Clean tasks */
+
+//Clean the bower files
+gulp.task('clean-bower', function () {
+  return gulp.src(BOWER, { read: false })
+    .pipe(clean({ force: true }));
+});
+
+//Clean the npm files
+gulp.task('clean-npm', function () {
+  return gulp.src(NPM, { read: false })
+    .pipe(clean({ force: true }));
+});
+
 //Clean the build files
-gulp.task('clean', function () {
+gulp.task('clean-install', function () {
   return gulp.src(BUILD, { read: false })
-    .pipe(clean());
+    .pipe(clean({ force: true }));
 });
 
-gulp.task('server', function () {
-  server.run(['server.js']);
+//Clean the typings file
+gulp.task('clean-typings', function () {
+  return gulp.src(TYPINGS, { read: false })
+    .pipe(clean({ force: true }));
 });
 
 
-/* Default tasks */
+/* Install tasks */
+
 //Install bower and npm
 gulp.task('install', function () {
-  return gulp.src(['./bower.json', './package.json'])
+  return gulp.src(['../bower.json', '../package.json'])
     .pipe(install());
 });
 
+//Install typescript definitions
+gulp.task('tsd', ['install'], function () {
+  return gulp.src('./gulp_tsd.json')
+    .pipe(tsd());
+});
+
+//Copy files that don't need to be compiled
+gulp.task('copy-files', function () {
+  return gulp.src([SRC + 'index.html', SRC + 'config.js'])
+    .pipe(gulp.dest(BUILD));
+});
+
+//Run less conversion
+gulp.task('less', function () {
+  return gulp.src(SRC + '**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest(BUILD));
+});
+
+
+/* Concatenate tasks */
+
 //Concatenate vendor JS into one file
-gulp.task('vendorJS', function () {
+gulp.task('vendorJS', ['install'], function () {
   return gulp.src([
     BOWER + 'angular/angular.js',
     BOWER + 'angular-animate/angular-animate.js',
@@ -56,7 +97,7 @@ gulp.task('vendorJS', function () {
 });
 
 //Concatenate vendor CSS into one file
-gulp.task('vendorCSS', function () {
+gulp.task('vendorCSS', ['install'], function () {
   return gulp.src([
     BOWER + 'angular-material/angular-material.css',
     BOWER + 'angular-material-icons/angular-material-icons.css'
@@ -80,21 +121,17 @@ gulp.task('concat-modules', function () {
   });
 });
 
-//Copy files that don't need to be compiled
-gulp.task('copy-files', function () {
-  return gulp.src([SRC + 'index.html', SRC + 'config.js'])
-    .pipe(gulp.dest(BUILD));
-});
 
-//Run less conversion
-gulp.task('less', function () {
-  return gulp.src(SRC + '**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest(BUILD));
+/* Server tasks */
+
+//Run the server
+gulp.task('server', function () {
+  express.run(['../server.js']);
 });
 
 
 /* Task batchers */
 //gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'concat-modules', 'copy-files', 'less']);
-gulp.task('build', ['vendorJS', 'vendorCSS']);
+gulp.task('clean', ['clean-install', 'clean-typings']);
+gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'tsd']);
 gulp.task('default', ['server']);
