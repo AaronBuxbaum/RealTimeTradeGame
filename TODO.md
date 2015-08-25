b34a6a5de922c3bf27026dab666bb5bfdad81c08
Aaron Buxbaum <abuxbaum@factset.com>
2015-08-25 03:29:00 -0400

    begin implementing the ticker service in the node server

--- TODO.md
+++ TODO.md
@@ -1,10 +1,9 @@
 To Do:
 
-Enhancements:
-1. Make stocks/add-new only pull NYSE stocks
-
 Core Features:
-1. Move the stock value server to a node.js server process so that it can be disconnected
+1. Finish stock value ticker in server.js
+  - Find a way for the results of that ticker to interact in real-time with Angular
+  - Once that's done, the ticker functionality in database-player-fact.js can be removed
 2. Database system -- how does the website communicate with the database?
 3. Data -- how do we avoid having too much at any given time?
   - Perhaps with a nightly/weekly/monthly daemon that reduces down the data
@@ -13,3 +12,4 @@ Core Features:
 
 Bugs:
 1. Sometimes the options in the list don't have values, which could cause problems if they're added to a portfolio
+2. Font doesn't seem to be working properly
--- server.js
+++ server.js
@@ -1,7 +1,74 @@
 var express = require('express');
-var app = express();
+var server = express();
+var _ = require('./bower_components/lodash/lodash.js');
 
-app.use(express.static(__dirname));
+//TODO: replace all of this example code with a real database
+var examplePlayers = [
+	{
+		portfolio: [
+			{
+				symbol: 'SPY',
+				percentage: 100
+			}
+		],
+		data: []
+	},
+	{
+		portfolio: [],
+		data: []
+	}
+];
 
-app.listen(8080);
-console.log("App listening on port 8080");
+function tickPortfolio(portfolio) {
+	var symbols = _.pluck(portfolio, 'symbol');
+	return {
+		data: [
+			{
+				l: symbols.length * Math.random() * 1000
+			}
+		]
+	};
+}
+
+function updatePortfolios() {
+	_.forEach(examplePlayers, function (player) {
+		var response = tickPortfolio(player.portfolio);
+		
+		//Get last tick's earnings or initialize to $1M
+		var lastTick = _.last(_.last(player.data)) || 1000000;
+			
+		//Update shares
+		_.forEach(player.portfolio, function (stock, index) {
+			stock.shares = (lastTick * (stock.percentage / 100)) / response.data[index].l;
+		});
+			
+		//Find how much is held in currency
+		var unusedPercentage = 100 - _.sum(player.portfolio, 'percentage');
+		var total = lastTick * (unusedPercentage / 100);
+
+		//Find new earnings
+		if (player.portfolio.length > 0) {
+			total += _.reduce(response.data, function (total, stockData, index) {
+				return total + stockData.l * player.portfolio[index].shares;
+			}, 0);
+		}
+		
+		//Round to avoid Javascript numeric bugs (ie. 1000.00000001)
+		//TODO: there's gotta be a cleaner way to do this
+		total = Number(total.toPrecision(2));
+
+		//Save to database
+		player.data.push([Date.now(), total]);
+	});
+}
+
+setInterval(updatePortfolios, 5000);
+
+function listen(port) {
+	server.use(express.static(__dirname));
+	server.listen(port);
+	console.log("Server listening on port", port);
+}
+
+updatePortfolios();
+listen(8080);
