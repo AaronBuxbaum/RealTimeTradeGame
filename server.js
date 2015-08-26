e3130cc7e1ca4cb43607e0fedd1eaff582146f09
Aaron Buxbaum <abuxbaum@factset.com>
2015-08-26 16:40:08 -0400

    working on the server some more

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var _ = require('lodash');
 
 //Initialize variables
 var portfolioUpdater;
-var FETCH_INTERVAL = 20000;
-var examplePlayerDB = new Firebase("https://realtimetrade.firebaseio.com/examplePlayer");
+var FETCH_INTERVAL = 5000;
+var examplePlayer = new Firebase("https://realtimetrade.firebaseio.com/examplePlayer");
+var portfolio = examplePlayer.child('portfolio');
 
 //Start the portfolio updater
 function startPortfolioUpdater() {
@@ -18,21 +19,40 @@ function startPortfolioUpdater() {
 
 //Update player's portfolio
 function updatePortfolio() {
-	examplePlayerDB.once('value', function (data) {
-		var player = data.val();
-
-		if (!player) { return; }
+	portfolio.once('value', function (p) {
+		if (!p) { return; }
 		
 		//Param must exist for getStockValues to return correctly, so if empty, I use SPY and throw it away
-		var symbols = (player.portfolio && player.portfolio.length > 0) ? _.pluck(player.portfolio, 'symbol') : ['SPY'];
+		var portfolioArray = _.toArray(p.val());
+		var symbols = (portfolioArray && portfolioArray.length > 0) ? _.pluck(portfolioArray, 'symbol') : ['SPY'];
+
+		//Push the new earnings to the database
+		getStockPrices(symbols).then(function (stockValues) {
+			var data = examplePlayer.child('data');
+			data.once('value', function (history) {
+				var previousEarnings = _.last(_.last(_.toArray(history.val())));
+				getPortfolioValue(portfolio, previousEarnings, stockValues).then(function (portfolioValue) {
+					console.log(portfolioValue);
+				});
+				//data.push([Date.now(), portfolioValue]);
+			});
+		});
+	});
+	
+	/*
+	examplePlayer.once('value', function (data) {
+		
 		
 		//Push the new earnings to the database
 		getStockPrices(symbols).then(function (stockValues) {
 			var previousEarnings = _.last(_.last(player.data));
 			var portfolioValue = getPortfolioValue(player.portfolio, previousEarnings, stockValues);
+			console.log(portfolioValue);
 			examplePlayerDB.child('data').push([Date.now(), portfolioValue]);
+			console.log('pushed');
 		});
 	});
+	*/
 }
 
 //Stop the portfolio updater
@@ -61,32 +81,44 @@ function getStockPrices(symbols) {
 
 //Return the value of the player's portfolio
 function getPortfolioValue(portfolio, previousEarnings, stockValues) {
+	var deferred = Q.defer();
+	
 	//If this is the first entry, initialize to $1M
 	if (!previousEarnings) {
 		previousEarnings = 1000000;
 	}
 			
 	//Find how much is held in currency
-	var unusedPercentage = 100 - _.sum(portfolio, 'percentage');
-	var total = previousEarnings * (unusedPercentage / 100);
+	portfolio.on('value', function (portfolioArray) {
+		portfolioArray = _.toArray(portfolioArray.val());
 
-	//Find new earnings
-	if (unusedPercentage < 100) {
-		total += _.reduce(stockValues, function (total, stockValue, i) {
-			if (!portfolio[i].shares) {
-				portfolio[i].shares = getNumberOfShares(previousEarnings, portfolio[i].percentage, stockValue);
-			}
+		var unusedPercentage = 100 - _.sum(portfolioArray, 'percentage');
+		var total = previousEarnings * (unusedPercentage / 100);
 
-			return total + Number(stockValue) * portfolio[i].shares;
-		}, 0);
-	}
+		//Find new earnings
+		if (unusedPercentage < 100) {
+			total += _.reduce(stockValues, function (total, stockValue, i) {
+				var stock = portfolioArray[i];
+
+				if (!stock.shares) {
+					stock.shares = getNumberOfShares(previousEarnings, stock.percentage, stockValue)
+					portfolio.update(stock);
+				}
+
+				return total + Number(stockValue) * stock.shares;
+			}, 0);
+		}
 	
-	//Update share counts
-	_.times(portfolio.length, function (i) {
-		portfolio[i].shares = getNumberOfShares(previousEarnings, portfolio[i].percentage, stockValues[i]);
+		//Update share counts
+		_.times(portfolioArray.length, function (i) {
+			portfolioArray[i].shares = getNumberOfShares(previousEarnings, portfolioArray[i].percentage, stockValues[i]);
+			portfolio.update(portfolioArray[i]);
+		});
+
+		deferred.resolve(roundNumber(total));
 	});
 
-	return roundNumber(total);
+	return deferred.promise;
 }
 
 //Find the number of shares given the total money, percentage allocated to this specific stock, and the value of a share
@@ -102,10 +134,13 @@ function roundNumber(num) {
 //Open a port and serve pages through it
 function listen(port) {
 	server.use(express.static(__dirname));
+	server.all('/*', function (request, response) {
+		return response.redirect('/src/');
+	});
 	server.listen(port);
 	console.log("Server listening on port", port);
 }
 
 //Start the server
-startPortfolioUpdater();
 listen(process.env.PORT || 8080);
+startPortfolioUpdater();
