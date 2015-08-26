f54492f22c1fbb322baa9517bf2e3675ace30701
Aaron Buxbaum <AaronBuxbaum@gmail.com>
2015-08-26 09:05:50 -0400

    update gulp to play nice with heroku

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,6 +4,7 @@ var requireDir = require('require-dir')();
 
 /* Task batchers */
 //gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'concat-modules', 'copy-files', 'less']);
+gulp.task('heroku:product', ['install', 'server']);
 gulp.task('clean', ['clean-build', 'clean-typings']);
 gulp.task('build', ['install', 'concat-vendor-js', 'concat-vendor-css', 'tsd']);
 gulp.task('default', ['server']);
