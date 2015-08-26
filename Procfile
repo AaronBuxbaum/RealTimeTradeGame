3fa2060f232585d0b3dd4d0f07b6f9ebb8c4da0d
Aaron Buxbaum <abuxbaum@factset.com>
2015-08-26 11:59:39 -0400

    another attempt for heroku to work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node node_modules/gulp/bin/gulp build --cwd gulp
+web: node node_modules/gulp/bin/gulp deploy --cwd gulp
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,6 +1,7 @@
 /* Dependencies */
 var gulp = require('gulp');
 var requireDir = require('require-dir')();
+var runSequence = require('run-sequence');
 
 /* Task batchers */
 //gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'concat-modules', 'copy-files', 'less']);
@@ -8,3 +9,7 @@ var requireDir = require('require-dir')();
 gulp.task('clean', ['clean-build', 'clean-typings']);
 gulp.task('build', ['install', 'concat-vendor-js', 'concat-vendor-css', 'tsd']);
 gulp.task('default', ['server']);
+
+gulp.task('deploy', function (callback) {
+	runSequence('install', ['concat-vendor-js', 'concat-vendor-css', 'tsd'], 'server', callback);
+});
