c65b0abe0046661b453d838f7fa47df09b3ce83a
Aaron Buxbaum <abuxbaum@factset.com>
2015-08-26 11:21:05 -0400

    update gulp task so that heroku doesn't crash

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,7 +4,8 @@ var requireDir = require('require-dir')();
 
 /* Task batchers */
 //gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'concat-modules', 'copy-files', 'less']);
-gulp.task('heroku:product', ['install', 'server']);
+//gulp.task('heroku:product', ['install', 'server']);
 gulp.task('clean', ['clean-build', 'clean-typings']);
 gulp.task('build', ['install', 'concat-vendor-js', 'concat-vendor-css', 'tsd']);
 gulp.task('default', ['server']);
+gulp.task('deploy', ['build', 'server']);
