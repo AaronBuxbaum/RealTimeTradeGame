3e3a9fe6aba210684546d8d4309987930179bc73
Aaron Buxbaum <abuxbaum@factset.com>
2015-08-30 21:34:33 -0400

    about dialog

--- src/about/about-ctrl.js
+++ src/about/about-ctrl.js
@@ -12,6 +12,16 @@ About.controller('AboutCtrl', function ($mdDialog, $templateCache) {
                 $scope.closeDialog = function () {
                     $mdDialog.hide();
                 }
+
+                $scope.developers = [
+                    {
+                        name: 'Aaron Buxbaum',
+                        title: 'Lead Software Engineer',
+                        picture: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/01c/0ac/0ece994.jpg',
+                        linkedIn: 'https://www.linkedin.com/in/aaronbuxbaum',
+                        text: 'I\'m a full-stack Software Engineer II at FactSet Research Systems, specializing in AngularJS. I hold a Bachelors of Science in Computer Science and a Minor in Mathematics from the University of Massachusetts-Amherst.'
+                    }
+                ];
             }
         });
     };
