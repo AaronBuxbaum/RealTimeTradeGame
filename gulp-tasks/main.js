4baa639b276e4afcd509221db97dcf79cf44a1ef
Aaron Buxbaum <abuxbaum@factset.com>
2015-09-03 09:57:29 -0400

    attempt to remove procfile for heroku buildpack

--- gulp-tasks/main.js
+++ gulp-tasks/main.js
@@ -4,7 +4,6 @@ var runSequence = require('run-sequence');
 
 /* Main tasks */
 //gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'concat-modules', 'copy-files', 'less']);
-//gulp.task('heroku:product', ['install', 'server']);
 gulp.task('clean', ['clean-npm', 'clean-build', 'clean-typings']);
 gulp.task('build', ['install-npm', 'install-tsd', 'concat-vendor-js', 'concat-vendor-css']);
 gulp.task('default', ['server']);
@@ -12,3 +11,5 @@ gulp.task('default', ['server']);
 gulp.task('deploy', function (callback) {
 	runSequence('install-npm', ['concat-vendor-js', 'concat-vendor-css'], 'server', callback);
 });
+
+gulp.task('heroku:product', ['deploy']);
