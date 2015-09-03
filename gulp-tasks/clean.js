a9d4767d74211500bfdf7925b1f94500df0bc501
Aaron Buxbaum <AaronBuxbaum@gmail.com>
2015-09-03 02:08:29 -0400

    use gulp del module for cleaning directories

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -1,21 +1,21 @@
 /* Dependencies */
 var gulp = require('gulp');
-var rimraf = require('rimraf');
+var del = require('del');
 var g = require('./global.json');
 
 /* Clean tasks */
 
 //Clean the node_modules directory
 gulp.task('clean-npm', function () {
-  return rimraf(g.NPM);
+  return del(g.NPM);
 });
 
 //Clean the build directory
 gulp.task('clean-build', function () {
-  return rimraf(g.BUILD);
+  return del(g.BUILD);
 });
 
 //Clean the typings directory
 gulp.task('clean-typings', function () {
-  return rimraf(g.TYPINGS);
+  return del(g.TYPINGS);
 });
