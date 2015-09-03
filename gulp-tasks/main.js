8a72fe33adcaae2680854e1666c64042a23c09f9
Aaron Buxbaum <AaronBuxbaum@gmail.com>
2015-09-03 01:46:38 -0400

    remove unused gulp task

--- gulp-tasks/main.js
+++ gulp-tasks/main.js
@@ -6,9 +6,9 @@ var runSequence = require('run-sequence');
 //gulp.task('build', ['install', 'vendorJS', 'vendorCSS', 'concat-modules', 'copy-files', 'less']);
 //gulp.task('heroku:product', ['install', 'server']);
 gulp.task('clean', ['clean-build', 'clean-typings']);
-gulp.task('build', ['install', 'concat-vendor-js', 'concat-vendor-css', 'tsd']);
+gulp.task('build', ['install', 'concat-vendor-js', 'concat-vendor-css']);
 gulp.task('default', ['server']);
 
 gulp.task('deploy', function (callback) {
-	runSequence('install', ['concat-vendor-js', 'concat-vendor-css', 'tsd'], 'server', callback);
+	runSequence('install', ['concat-vendor-js', 'concat-vendor-css'], 'server', callback);
 });
