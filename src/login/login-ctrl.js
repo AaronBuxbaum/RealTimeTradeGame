ab8cc0a9218f845eeda6a511c223f3c14e471212
Aaron Buxbaum <abuxbaum@factset.com>
2015-09-07 00:49:26 -0400

    change to compile system

--- src/login/login-ctrl.js
+++ src/login/login-ctrl.js
@@ -1,13 +1,17 @@
 Login.controller('LoginCtrl', function ($mdDialog) {
-    $mdDialog.show({
-        controller: function (AuthenticationService) {
-            var ctrl = this;
-            ctrl.logIn = AuthenticationService.logIn;
-            ctrl.signUp = AuthenticationService.signUp;
-        },
-        controllerAs: 'ctrl',
-        bindToController: true,
-        templateUrl: 'login/login.html',
-        escapeToClose: false
-    });
+    var ctrl = this;
+
+    ctrl.showDialog = function () {
+        $mdDialog.show({
+            controller: function (AuthenticationService) {
+                var ctrl = this;
+                ctrl.logIn = AuthenticationService.logIn;
+                ctrl.signUp = AuthenticationService.signUp;
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: 'login/login.html',
+            escapeToClose: false
+        });
+    };
 });
--- src/login/login-drct.js
+++ src/login/login-drct.js
@@ -1,9 +1,21 @@
-Login.directive('login', function () {
+Login.directive('login', function ($mdDialog) {
 	return {
 		restrict: 'E',
 		controller: 'LoginCtrl',
 		controllerAs: 'ctrl',
 		bindToController: true,
-		scope: {}
+		scope: {},
+		compile: function () {
+			return {
+				pre: function (scope) {
+					scope.$on('$destroy', function () {
+						$mdDialog.hide();
+					});
+				},
+				post: function (scope, elem, attrs, ctrl) {
+					ctrl.showDialog();
+				}
+			}
+		}
 	};
 });
