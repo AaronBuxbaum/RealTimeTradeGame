70f6c74a14d162b3f89cc586488ae3ca7e29a40c
Aaron Buxbaum <abuxbaum@factset.com>
2015-09-07 00:15:59 -0400

    change log in to a modal

--- src/login/login-ctrl.js
+++ src/login/login-ctrl.js
@@ -1,6 +1,13 @@
-Login.controller('LoginCtrl', function (AuthenticationService) {
-    var ctrl = this;
-
-    ctrl.logIn = AuthenticationService.logIn;
-    ctrl.signUp = AuthenticationService.signUp;
+Login.controller('LoginCtrl', function ($mdDialog) {
+    $mdDialog.show({
+        controller: function (AuthenticationService) {
+            var ctrl = this;
+            ctrl.logIn = AuthenticationService.logIn;
+            ctrl.signUp = AuthenticationService.signUp;
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        templateUrl: 'login/login.html',
+        escapeToClose: false
+    });
 });
--- src/login/login-drct.js
+++ src/login/login-drct.js
@@ -1,7 +1,6 @@
 Login.directive('login', function () {
 	return {
 		restrict: 'E',
-		templateUrl: 'login/login.html',
 		controller: 'LoginCtrl',
 		controllerAs: 'ctrl',
 		bindToController: true,
