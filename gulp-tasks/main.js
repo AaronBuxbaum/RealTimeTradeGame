8ec1c0ac414a7054928060f1195b5b21081ab213
Aaron Buxbaum <abuxbaum@factset.com>
2015-09-09 15:01:33 -0400

    don't install typescripts for deployments

--- gulp-tasks/main.js
+++ gulp-tasks/main.js
@@ -7,6 +7,6 @@ gulp.task('build', ['install', 'concat-vendor-js', 'concat-vendor-css']);
 gulp.task('default', ['server']);
 
 gulp.task('deploy', function (callback) {
-	runSequence('install', ['concat-vendor-js', 'concat-vendor-css'], 'server', callback);
+	runSequence(['install-npm', 'install-bower'], ['concat-vendor-js', 'concat-vendor-css'], 'server', callback);
 	//['concat-modules', 'copy-files', 'less']);
 });
