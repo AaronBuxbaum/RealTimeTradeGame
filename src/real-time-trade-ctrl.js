3500a1eb4a523adfdbbb8ba435c04855b2e8a44f
Aaron Buxbaum <abuxbaum@factset.com>
2015-09-09 12:07:39 -0400

    remove unused code

--- src/real-time-trade-ctrl.js
+++ src/real-time-trade-ctrl.js
@@ -1,11 +1,6 @@
 angular.module('RealTimeTrade').controller('RealTimeTradeCtrl', function (AuthenticationService) {
     var ctrl = this;
-
-	ctrl.leagueName = 'Example League';
 	
-	//TODO: why doesn't this work?
-	//ctrl.isAuthenticated = AuthenticationService.isAuthenticated;
-
 	//Changes the object when authentication state changes
 	AuthenticationService.auth.$onAuth(function (authState) {
 		ctrl.isAuthenticated = !!authState;
