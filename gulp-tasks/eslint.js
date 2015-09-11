291b29cc795720f87ea81af678fadccfdef95161
Aaron Buxbaum <AaronBuxbaum@gmail.com>
2015-09-11 00:50:32 -0400

    add ESLint task

---
+++ gulp-tasks/eslint.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var g = require('./global.json');
+
+gulp.task('lint', function () {
+    return gulp.src([g.SRC + '**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
