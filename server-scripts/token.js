7571686a41490dcb54dd166fbf8a2446895dc2df
Aaron Buxbaum <abuxbaum@factset.com>
2015-09-11 15:42:30 -0400

    more token work

--- server-scripts/token.js
+++ server-scripts/token.js
@@ -1,26 +1,25 @@
 var Q = require('q');
 var FirebaseTokenGenerator = require('firebase-token-generator');
-var tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET || '');
 
 function authenticate(ref, uid) {
-	var defer = Q.defer();
+	if (!process.env.FIREBASE_SECRET) {
+		return;
+	}
 
-	ref.authWithCustomToken(createToken(uid),
+	var defer = Q.defer();
+	var tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET);
+	var token = tokenGenerator.createToken({ uid: uid });
+
+	ref.authWithCustomToken(token,
 		function (error) {
 			if (!error) {
 				defer.resolve();
 			} else {
 				defer.reject(error);
 			}
-		}, {
-			remember: 'none'
 		});
 
 	return defer.promise;
-}
-
-function createToken (uid) {
-	return tokenGenerator.createToken({ uid: uid });
 }
 
 module.exports.authenticate = authenticate;
