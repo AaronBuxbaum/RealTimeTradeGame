2f67c37cf32cc3855d21b3d74af025c3ac46746d
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-17 09:28:06 -0500

    Get login to reappear when failed

--- src/auth/auth-fact.js
+++ src/auth/auth-fact.js
@@ -1,70 +1,68 @@
 
 angular.module('Authentication').factory('AuthenticationService', function ($timeout, $firebaseAuth, $firebaseObject) {
-	var svc = this;
+  var svc = this;
 
-	var ref = new Firebase('https://realtimetrade.firebaseio.com');
-	svc.auth = $firebaseAuth(ref);
+  var ref = new Firebase('https://realtimetrade.firebaseio.com');
+  svc.auth = $firebaseAuth(ref);
 
-	//Get signed in user ID (or null if not signed in)
-	svc.getUserID = function () {
-		return svc.auth.$getAuth().uid;
-	};
+  //Get signed in user ID (or null if not signed in)
+  svc.getUserID = function () {
+    return svc.auth.$getAuth().uid;
+  };
 	
-	//Watch for changes in the authentication state
-	svc.auth.$onAuth(function (authData) {
-		$timeout(function () {
-			svc.auth.data = authData;
-		});
-	});
+  //Watch for changes in the authentication state
+  svc.auth.$onAuth(function (authData) {
+    svc.auth.data = authData;
+  });
 
-	//Log in
-	svc.logIn = function (email, password) {
-		svc.auth.isLoading = true;
-		return svc.auth.$authWithPassword({
-            email: email,
-            password: password
-        })
-			.then(function (response) {
-				svc.profileImage = response.password.profileImageURL;
-				return svc.profileImage;
-			})
+  //Log in
+  svc.logIn = function (email, password) {
+    svc.auth.isLoading = true;
+    return svc.auth.$authWithPassword({
+      email: email,
+      password: password
+    })
+      .then(function (response) {
+        svc.profileImage = response.password.profileImageURL;
+        return svc.profileImage;
+      })
 
-			.catch(function (error) {
-				console.log(error);
-				return error;
-			})
+      .catch(function (error) {
+        console.log(error);
+        return error;
+      })
 
-			.finally(function () {
-				svc.auth.isLoading = false;
-			});
-	};
+      .finally(function () {
+        svc.auth.isLoading = false;
+      });
+  };
 
-	//Sign up
-	svc.signUp = function (email, password) {
-		svc.auth.$createUser({
-			email: email,
-			password: password
-		})
-			.then(function (response) {
-				var user = ref.child('users').child(response.uid);
-				var newUser = $firebaseObject(user);
-				newUser.uid = response.uid;
-				newUser.name = response.uid;
-				newUser.league = 1; //TODO: remove -- this is just for early convenience factor
-				newUser.$save();
-				return svc.logIn(email, password);
-			})
+  //Sign up
+  svc.signUp = function (email, password) {
+    svc.auth.$createUser({
+      email: email,
+      password: password
+    })
+      .then(function (response) {
+        var user = ref.child('users').child(response.uid);
+        var newUser = $firebaseObject(user);
+        newUser.uid = response.uid;
+        newUser.name = response.uid;
+        newUser.league = 1; //TODO: remove -- this is just for early convenience factor
+        newUser.$save();
+        return svc.logIn(email, password);
+      })
 
-			.catch(function (error) {
-				console.log(error);
-				return error;
-			});
-	};
+      .catch(function (error) {
+        console.log(error);
+        return error;
+      });
+  };
 	
-	//Log out
-	svc.logOut = function () {
-		svc.auth.$unauth();
-	};
+  //Log out
+  svc.logOut = function () {
+    svc.auth.$unauth();
+  };
 
-	return svc;
+  return svc;
 });
