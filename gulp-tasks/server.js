499dd0c24f9edf0e0c2056efd186e5abbfecc963
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-18 17:27:30 -0500

    Remove livereload on production

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -7,6 +7,10 @@ gulp.task('server', function () {
   var port = process.env.PORT || 8080;
   var server = gls.static('build', port);
 
+  if (process.env.NODE_ENV === 'production') {
+    server.config.livereload = false;
+  }
+
   require('connect-livereload')();
   require('../server-scripts/portfolio-updater.js').initialize();
 
