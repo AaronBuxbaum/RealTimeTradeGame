696cf84825fffff57e048a9016a2d8fab79dea99
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-18 17:38:09 -0500

    Another attempt at getting rid of livereload error

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -8,10 +8,9 @@ gulp.task('server', function () {
   var server = gls.static('build', port);
 
   if (process.env.NODE_ENV === 'production') {
-    server.config.livereload = false;
+    require('connect-livereload')();
   }
 
-  require('connect-livereload')();
   require('../server-scripts/portfolio-updater.js').initialize();
 
   server.start();
