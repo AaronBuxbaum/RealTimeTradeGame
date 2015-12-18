8ac4809900889189df9ca51ac2d292ea25eb0241
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-18 18:31:23 -0500

    Fix typo

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -5,7 +5,7 @@ var gls = require('gulp-live-server');
 //Run the server
 gulp.task('server', function () {
   var port = process.env.PORT || 8080;
-  var server = gls([gls.script, 'build', port], { env: { NODE_ENV: 'development' } });
+  var server = gls([gls.script, 'build', port], { env: { NODE_ENV: 'production' } });
 
   require('connect-livereload')();
   require('../server-scripts/portfolio-updater.js').initialize();
