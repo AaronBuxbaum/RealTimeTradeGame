be5ff9c184b9b8788ff1c5e453743958b33e652a
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-18 14:46:05 -0500

    Set up livereload server

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -7,6 +7,7 @@ gulp.task('server', function () {
   var port = process.env.PORT || 8080;
   var server = gls([gls.script, 'build', port], undefined, 35729);
 
+  require('connect-livereload')();
   require('../server-scripts/portfolio-updater.js').initialize();
 
   server.start();
