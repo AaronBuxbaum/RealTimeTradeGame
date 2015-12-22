002881ce31f3f01e9cadb79f211681682fb3a1fc
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-22 13:48:38 -0500

    Update travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,3 @@
-var glob = require('glob');
-
-var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
 exports.config = {
-	//seleniumAddress: 'http://localhost:4444/wd/hub',
-	seleniumServerJar: files[0],
-	specs: ['e2e-tests/**/*.spec.js']
+  specs: ['e2e-tests/**/*.spec.js']
 };
