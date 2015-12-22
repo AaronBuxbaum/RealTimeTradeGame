4751c0a7c85e80c12b18e383e7a6108f235ac07f
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-22 14:59:07 -0500

    Revert previous commits

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,3 +1,8 @@
+var glob = require('glob');
+
+var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
 exports.config = {
-  specs: ['e2e-tests/**/*.spec.js']
+	//seleniumAddress: 'http://localhost:4444/wd/hub',
+	seleniumServerJar: files[0],
+	specs: ['e2e-tests/**/*.spec.js']
 };
