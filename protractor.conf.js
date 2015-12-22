16851078d8e08bcf7961c818900b160d4ff9a695
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-22 15:34:21 -0500

    Add direct connect

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,7 @@ var glob = require('glob');
 
 var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
 exports.config = {
+	directConnect: true,
 	//seleniumAddress: 'http://localhost:4444/wd/hub',
 	seleniumServerJar: files[0],
 	specs: ['e2e-tests/**/*.spec.js']
