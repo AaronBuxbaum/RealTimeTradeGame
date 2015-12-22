5c2bbe01f8768dd9fdad2ffe2632539518a1852b
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-22 16:02:50 -0500

    Another e2e attempt

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,9 +1,20 @@
-var glob = require('glob');
+//var glob = require('glob');
 
-var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
+//var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
 exports.config = {
-	directConnect: true,
 	//seleniumAddress: 'http://localhost:4444/wd/hub',
-	seleniumServerJar: files[0],
-	specs: ['e2e-tests/**/*.spec.js']
+	//seleniumServerJar: files[0],
+	specs: ['e2e-tests/**/*.spec.js'],
+
+	sauceLabs: {
+		testName: 'Web App Unit Tests',
+		//tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+		username: process.env.SAUCE_USERNAME,
+		accessKey: process.env.SAUCE_ACCESS_KEY,
+		startConnect: false,
+		connectOptions: {
+			port: 5757,
+			logfile: 'sauce_connect.log'
+		}
+	}
 };
