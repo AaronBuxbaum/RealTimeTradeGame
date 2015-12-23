89e879363de8e08dd4732c2afbc5ddc64cb3f668
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-23 17:56:55 -0500

    Check if this works on sauce

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -1,21 +1,22 @@
 /* Dependencies */
 var gulp = require('gulp');
 var karma = require('karma');
-var protractor = require('gulp-protractor');
+var protractor = require('gulp-angular-protractor');
 
 gulp.task('test', ['unit-tests', 'e2e-tests']);
 
 gulp.task('unit-tests', function (done) {
-	return new karma.Server({
-		configFile: __dirname + '/../karma.conf.js',
-		singleRun: true
-	}, done).start();
+  return new karma.Server({
+    configFile: __dirname + '/../karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
-gulp.task('e2e-tests', function () {
-	return gulp
-		.src(['./e2e-tests/**/*.js'])
-		.pipe(protractor.protractor({
-			configFile: './protractor.conf.js'
-		}));
-});
+gulp.task('protractor', function () {
+  return gulp
+    .src(['./e2e-tests/**/*.js'])
+    .pipe(protractor({
+      configFile: './protractor.conf.js'
+    }))
+    .on('error', function (e) { throw e });
+})
--- protractor.conf.js
+++ protractor.conf.js
@@ -2,19 +2,20 @@
 
 //var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
 exports.config = {
-	//seleniumAddress: 'http://localhost:4444/wd/hub',
-	//seleniumServerJar: files[0],
-	specs: ['e2e-tests/**/*.spec.js'],
+  directConnect: true,
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumServerJar: files[0],
+  specs: ['e2e-tests/**/*.spec.js'],
 
-	sauceLabs: {
-		testName: 'Web App Unit Tests',
-		tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-		username: process.env.SAUCE_USERNAME,
-		accessKey: process.env.SAUCE_ACCESS_KEY,
-		startConnect: false,
-		connectOptions: {
-			port: 55547,
-			logfile: 'sauce_connect.log'
-		}
-	}
+  sauceLabs: {
+    testName: 'Web App Unit Tests',
+    tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+    username: process.env.SAUCE_USERNAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY,
+    startConnect: false,
+    connectOptions: {
+      port: 55547,
+      logfile: 'sauce_connect.log'
+    }
+  }
 };
