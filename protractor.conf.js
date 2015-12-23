6d6ed434cf8f08ef3dabac48685910fda9220a79
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-23 18:30:14 -0500

    Finally got protractor to work with me!

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,26 +1,8 @@
-//var glob = require('glob');
-
-//var files = glob.sync('./node_modules/protractor/selenium/selenium-server-standalone-*.jar', { nodir: true });
 exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
-
-  //directConnect: true,
-  
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-  
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: files[0],
-
   specs: ['e2e-tests/**/*.spec.js'],
-  /*
-  sauceLabs: {
-    tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-    username: process.env.SAUCE_USERNAME,
-    accessKey: process.env.SAUCE_ACCESS_KEY,
-    startConnect: false
-  }
-  */
 };
