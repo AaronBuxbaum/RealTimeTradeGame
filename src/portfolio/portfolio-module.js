e65315bf945c10ddbbc2497d1267cbd1c90e2cdd
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-24 13:35:00 -0500

    Add fade animations

--- src/portfolio/portfolio-module.js
+++ src/portfolio/portfolio-module.js
@@ -5,12 +5,14 @@
 * @description
 * The Portfolio module.
 *
+* @requires ngAnimate
 * @requires ngMaterial
 * @requires ngMdIcons
 * @requires AddNewStock
 * @requires StockSlider
 **/
 angular.module('Portfolio', [
+	'ngAnimate',
 	'ngMaterial',
 	'ngMdIcons',
 	'AddNewStock',
