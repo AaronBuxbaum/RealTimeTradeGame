29e46185b86e2f2a050f3375bda4e878f3f5237a
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-24 15:19:34 -0500

    Add loading text

--- src/ticker/ticker-ctrl.js
+++ src/ticker/ticker-ctrl.js
@@ -23,6 +23,7 @@ angular.module('Ticker').controller('TickerCtrl', function ($q, $http, Authentic
     var chartOptions = json.data['chart-options'] || json.data['ticker/chart-options'];
     chartOptions.chart.renderTo = $('#stockTicker')[0];
     ctrl.chart = new Highcharts.StockChart(chartOptions);
+    ctrl.chart.showLoading();
     ctrl.renderChart = _.debounce(ctrl.chart.redraw, 10000);
     return $q.when();
   }
@@ -36,6 +37,7 @@ angular.module('Ticker').controller('TickerCtrl', function ($q, $http, Authentic
       data.push(value.val());
     });
     line.setData(data);
+    ctrl.chart.hideLoading();
           
     //Update lines as new values come in
     seriesData.ref().limitToLast(1).on('child_added', function (point) {
@@ -43,50 +45,4 @@ angular.module('Ticker').controller('TickerCtrl', function ($q, $http, Authentic
       ctrl.renderChart();
     });
   }
-
-  /*
-    //Find the active league for the active user
-    function findActiveLeague(activeUser) {
-      if (!activeUser.val() || !activeUser.val().league) {
-        return;
-      }
-  
-      ref.child('leagues').once('value', function (leagues) {
-        var findLeague = _.find(leagues.val(), { id: activeUser.val().league });
-        if (!findLeague) {
-          return;
-        }
-  
-        if (findLeague.endTime) {
-          ctrl.endTime = findLeague.endTime;
-        }
-  
-        _.forEach(findLeague.users, renderUsers);
-      });
-    }
-
-    //Render changes to the stock grid
-    function renderUsers(leagueUser, i) {
-      ref.child('users').child(leagueUser.toString()).once('value', function (userRef) {
-        var user = userRef.val();
-
-        ref.child('series').child(user.uid).orderByChild('0').once('value', function (series) {
-          var line = ctrl.chart.addSeries(user);
-
-          //Set initial data
-          var data = [];
-          series.forEach(function (value) {
-            data.push(value.val());
-          });
-          line.setData(data, false);
-            
-          //Update lines as new values come in
-          series.ref().limitToLast(1).on('child_added', function (point) {
-            line.addPoint(point.val(), false);
-            ctrl.renderChart();
-          });
-        });
-      });
-    }
-  */
 });
