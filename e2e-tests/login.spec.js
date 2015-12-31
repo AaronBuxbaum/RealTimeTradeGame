963d281f98aacb66e030c924d0e0bad79fbb56b0
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-30 22:10:14 -0500

    Another attempt at e2e test

--- e2e-tests/login.spec.js
+++ e2e-tests/login.spec.js
@@ -1,20 +1,22 @@
-xdescribe('log in interface', function () {
+describe('log in interface', function () {
     it('should allow you to log in', function () {
         browser.get('localhost:8080');
+
+        var buttons = element(by.tagName('md-button'));
 
         //Fail for invalid email
         element(by.model('ctrl.email')).sendKeys('example');
         element(by.model('ctrl.password')).sendKeys('examplepassword');
-        element('md-button')[0].click();
+        buttons[0].click();
         
         //Fail for invalid password
         element(by.model('ctrl.email')).sendKeys('example@example.com');
         element(by.model('ctrl.password')).sendKeys('');
-        element('md-button')[0].click();
+        buttons[0].click();
         
         //Success
         element(by.model('ctrl.email')).sendKeys('example@example.com');
         element(by.model('ctrl.password')).sendKeys('examplepassword');
-        element('md-button')[0].click();
+        buttons[0].click();
     });
 });
