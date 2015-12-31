1fe1c53103413ed148545d3d9fe77eaac6d0dcf2
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-31 16:02:28 -0500

    Update protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,12 +1,12 @@
 exports.config = {
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  baseUrl: 'http://localhost:8080',
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-  specs: ['e2e-tests/**/*.spec.js'],
-  onPrepare: function () {
-    browser.driver.get(browser.baseUrl);
-  }
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    baseUrl: 'http://localhost:' + (process.env.PORT || 8080),
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+    specs: ['e2e-tests/**/*.spec.js'],
+    onPrepare: function () {
+        browser.driver.get(browser.baseUrl);
+    }
 };
