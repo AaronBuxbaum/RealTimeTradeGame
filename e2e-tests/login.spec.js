7fa4b27dd7cafd0a5bf64837f856ba9e61075614
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-30 21:31:55 -0500

    Example login e2e test
    Definitely won't work yet

---
+++ e2e-tests/login.spec.js
@@ -0,0 +1,20 @@
+describe('log in interface', function () {
+    it('should allow you to log in', function () {
+        browser.get('localhost:8080');
+
+        //Fail for invalid email
+        element(by.model('ctrl.email')).sendKeys('example');
+        element(by.model('ctrl.password')).sendKeys('examplepassword');
+        element('md-button')[0].click();
+        
+        //Fail for invalid password
+        element(by.model('ctrl.email')).sendKeys('example@example.com');
+        element(by.model('ctrl.password')).sendKeys('');
+        element('md-button')[0].click();
+        
+        //Success
+        element(by.model('ctrl.email')).sendKeys('example@example.com');
+        element(by.model('ctrl.password')).sendKeys('examplepassword');
+        element('md-button')[0].click();
+    });
+});
