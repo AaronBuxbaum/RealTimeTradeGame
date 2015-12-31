d99b29a31c8c7fd0944c9cf23f1dda91f6302e4e
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-31 00:52:32 -0500

    Coverage should use sourcemaps

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,31 @@
 module.exports = function (config) {
-	config.set({
-		autoWatch: false,
-		basePath: '',
-		browsers: ['PhantomJS'],
-		colors: true,
-		files: [
-			'build/vendors.js',
-			'build/templates.js',
-			'build/app.js',
-			'src/**/*.spec.js'
-		],
-		frameworks: ['jasmine'],
-		plugins: [
-			'karma-jasmine',
-			'karma-phantomjs-launcher',
-			'karma-coverage'
-		],
-		preprocessors: {
-			'src': 'coverage'
-		},
-		reporters: ['progress', 'coverage', 'coveralls'],
-		singleRun: true,
-		coverageReporter: {
-			type: 'lcov',
-			dir: 'coverage/'
-		}
-	});
+    config.set({
+        autoWatch: false,
+        basePath: '',
+        browsers: ['PhantomJS'],
+        colors: true,
+        files: [
+            'build/vendors.js',
+            'build/templates.js',
+            'build/app.js',
+            'src/**/*.spec.js'
+        ],
+        frameworks: ['jasmine'],
+        plugins: [
+            'karma-jasmine',
+            'karma-phantomjs-launcher',
+            'karma-sourcemap-loader',
+            'karma-coverage',
+            'karma-coveralls'
+        ],
+        reporters: ['progress', 'coverage', 'coveralls'],
+        preprocessors: {
+            'build/app.js': ['sourcemap', 'coverage']
+        },
+        singleRun: true,
+        coverageReporter: {
+            type: 'text',
+            dir: 'coverage/'
+        }
+    });
 };
