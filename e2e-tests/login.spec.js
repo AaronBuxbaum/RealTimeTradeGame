8d6bbdc4afd9267e88618d2e37a6acc3c3faad42
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-30 22:40:06 -0500

    Improve code style for buttons

--- e2e-tests/login.spec.js
+++ e2e-tests/login.spec.js
@@ -3,24 +3,24 @@ describe('log in interface', function () {
         browser.get('localhost:8080');
 
         var dialog = element(by.tagName('md-dialog'));
-        var buttons = element(by.tagName('md-button'));
+        var buttons = element.all(by.tagName('md-button'));
 
         //Fail for invalid email
         element(by.model('ctrl.email')).sendKeys('example');
         element(by.model('ctrl.password')).sendKeys('examplepassword');
-        buttons.click();
+        buttons[0].click();
         expect(dialog.isPresent()).toBe(true);
         
         //Fail for invalid password
         element(by.model('ctrl.email')).sendKeys('example@example.com');
         element(by.model('ctrl.password')).sendKeys('');
-        buttons.click();
+        buttons[0].click();
         expect(dialog.isPresent()).toBe(true);
         
         //Success
         element(by.model('ctrl.email')).sendKeys('example@example.com');
         element(by.model('ctrl.password')).sendKeys('examplepassword');
-        buttons.click();
+        buttons[0].click();
         expect(dialog.isPresent()).to.become(false);
     });
 });
