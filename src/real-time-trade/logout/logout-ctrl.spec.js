aa7aa46e71e82bce494f1d15aa4bb009d68fca21
Aaron Buxbaum <me@aaronbuxbaum.com>
2015-12-31 14:40:40 -0500

    Add unit test for log out controller

---
+++ src/real-time-trade/logout/logout-ctrl.spec.js
@@ -0,0 +1,22 @@
+describe('LogoutCtrl', function () {
+  var ctrl, scope, AuthenticationService;
+
+  beforeEach(function () {
+    module('Logout');
+  });
+
+  beforeEach(inject(function ($controller, $rootScope, _AuthenticationService_) {
+    scope = $rootScope.$new();
+    ctrl = $controller('LogoutCtrl', {
+      $scope: scope
+    });
+    AuthenticationService = _AuthenticationService_;
+  }));
+
+  describe('logOut', function () {
+    it('has a logOut function', function () {
+      expect(_.isFunction(ctrl.logOut)).toBeTruthy();
+      expect(ctrl.logOut).toBe(AuthenticationService.logOut);
+    });
+  });
+});
