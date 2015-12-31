45b8492c6d60fa0970d1eb19712d6cec26cbc6c3
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2015-12-31 11:52:27 -0500

    Add some tests to add new stock controller/directive

---
+++ src/portfolio/add-new-stock/add-new-stock-ctrl.spec.js
@@ -0,0 +1,40 @@
+describe('AddNewStockCtrl', function () {
+    var ctrl, PortfolioService;
+
+    beforeEach(function () {
+        module('AddNewStock');
+    });
+
+    beforeEach(inject(function ($controller, _PortfolioService_) {
+        ctrl = $controller('AddNewStockCtrl', {});
+        PortfolioService = _PortfolioService_;
+    }));
+
+    describe('addStock', function () {
+        it('has an addStock function', function () {
+            expect(_.isFunction(ctrl.addStock)).toBeTruthy();
+        });
+
+        it('calls the portfolio function and the close function', function () {
+            spyOn(PortfolioService, 'addStock');
+            spyOn(ctrl, 'close');
+            ctrl.addStock();
+            expect(PortfolioService.addStock).toHaveBeenCalled();
+            expect(ctrl.close).toHaveBeenCalled();
+        });
+    });
+
+    describe('close', function () {
+        it('has an close function', function () {
+            expect(_.isFunction(ctrl.close)).toBeTruthy();
+        });
+
+        it('nullifies the stock value and closes the dropdown', function () {
+            ctrl.isOpen = true;
+            ctrl.newStock = 'AAA';
+            ctrl.close();
+            expect(ctrl.isOpen).toBeFalsy();
+            expect(ctrl.newStock).toBeNull();
+        });
+    });
+});
---
+++ src/portfolio/add-new-stock/add-new-stock-drct.spec.js
@@ -0,0 +1,5 @@
+describe('AddNewStock', function () {
+    beforeEach(function () {
+        module('AddNewStock');
+    });
+});
