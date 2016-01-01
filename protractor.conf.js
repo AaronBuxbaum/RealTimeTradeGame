bd51b6b57cbcc98479bd109fa919a485a9834f5e
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-01 15:12:25 -0500

    Remove commented out code

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,22 +8,5 @@ exports.config = {
     framework: 'jasmine',
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
-    specs: ['e2e-tests/**/*.spec.js'],
-    onPrepare: function () {
-        /*
-            var dir,
-                wrench;
-
-            require('jasmine-reporters');
-
-            wrench = require('wrench');
-            dir = 'test/report/protractor/';
-            global.select = global.by;
-
-            wrench.mkdirSyncRecursive(dir);
-            jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter(dir, true, true));
-        */
-
-        //browser.driver.get(browser.baseUrl);
-    }
+    specs: ['e2e-tests/**/*.spec.js']
 };
