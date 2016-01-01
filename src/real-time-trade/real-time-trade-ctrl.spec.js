4422899d32096a3790dbb3e74b81cca4c9a83a41
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-01 17:36:14 -0500

    real time trade ctrl test

--- src/real-time-trade/real-time-trade-ctrl.spec.js
+++ src/real-time-trade/real-time-trade-ctrl.spec.js
@@ -1,13 +1,26 @@
 describe('RealTimeTradeCtrl', function () {
-    var ctrl;
+    var ctrl, AuthenticationService;
 
     beforeEach(function () {
         module('RealTimeTrade');
     });
 
-    beforeEach(inject(function ($controller, $rootScope) {
+    beforeEach(inject(function ($controller, $rootScope, _AuthenticationService_) {
         ctrl = $controller('RealTimeTradeCtrl', {
             $scope: $rootScope.$new()
         });
+
+        AuthenticationService = _AuthenticationService_;
     }));
+
+    describe('initialization', function () {
+        it('sets up the loading styles', function () {
+            expect(_.isObject(ctrl.loadingStyle)).toBeTruthy();
+        });
+
+        it('hooks up the authentication service', function () {
+            expect(ctrl.auth).toBeDefined();
+            expect(ctrl.auth).toEqual(AuthenticationService.auth);
+        });
+    });
 });
