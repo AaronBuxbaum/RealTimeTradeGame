5eb5f37500aefc0d03f9f999e56269b1437af8b0
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-01 16:54:53 -0500

    Ticker unit tests

--- src/ticker/ticker-ctrl.spec.js
+++ src/ticker/ticker-ctrl.spec.js
@@ -1,11 +1,44 @@
 describe('TickerCtrl', function () {
-    var ctrl;
+    var ctrl, createController, $httpBackend;
 
     beforeEach(function () {
         module('Ticker');
     });
 
-    beforeEach(inject(function ($controller) {
-        ctrl = $controller('TickerCtrl', {});
+    beforeEach(inject(function ($rootScope, $controller, _$httpBackend_, AuthenticationService) {
+        $httpBackend = _$httpBackend_;
+
+        createController = function () {
+            ctrl = $controller('TickerCtrl', { $scope: $rootScope.$new() });
+        };
+
+        Highcharts.StockChart = function () { return {
+            showLoading: function () {},
+            redraw: function () {}
+        } };
+
+        AuthenticationService.auth.data = {
+            uid: 'AAA'
+        };
+        
+        $httpBackend.whenGET('json.js').respond({
+            'chart-options': {
+                'chart': {}
+            }
+        });
     }));
+
+    describe('initialize', function () {
+        it('sets up the chart', function () {
+            createController();
+            $httpBackend.flush();
+            $httpBackend.expectGET('json.js');
+        });
+        
+        it('creates the chart', function () {
+            createController();
+            $httpBackend.flush();
+            expect(_.isObject(ctrl.chart)).toBeTruthy();
+        });
+    });
 });
