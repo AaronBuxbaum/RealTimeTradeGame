6c468528ecf20f852b110bf52e2bb0a945b102d8
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-01 17:04:46 -0500

    Add about unit tests

---
+++ src/real-time-trade/about/about-ctrl.spec.js
@@ -0,0 +1,26 @@
+describe('AboutCtrl', function () {
+    var ctrl, $mdDialog;
+
+    beforeEach(function () {
+        module('About');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope, _$mdDialog_) {
+        ctrl = $controller('AboutCtrl', {
+            $scope: $rootScope.$new()
+        });
+        $mdDialog = _$mdDialog_;
+    }));
+
+    describe('openAbout', function () {
+        it('has an openAbout', function () {
+            expect(_.isFunction(ctrl.openAbout)).toBeTruthy();
+        });
+
+        it('opens a dialog', function () {
+            spyOn($mdDialog, 'show');
+            ctrl.openAbout();
+            expect($mdDialog.show).toHaveBeenCalled();
+        });
+    });
+});
