12562ed669897961d7e6b9e7a1adfb14f20d69f7
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-01 18:13:38 -0500

    Stock slider unit tests

---
+++ src/portfolio/stock-slider/stock-slider-ctrl.spec.js
@@ -0,0 +1,20 @@
+describe('StockSliderCtrl', function () {
+    var ctrl, createController;
+
+    beforeEach(function () {
+        module('StockSlider');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        createController = function () {
+            ctrl = $controller('StockSliderCtrl', { $scope: $rootScope.$new() });
+        };
+    }));
+
+    describe('initialization', function () {
+        it('has a getMax function', function () {
+            createController();
+            expect(_.isFunction(ctrl.getMax)).toBeTruthy();
+        });
+    });
+});
