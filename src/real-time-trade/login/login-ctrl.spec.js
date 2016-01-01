b5fbb42e2f58da4b58b3cfc7e4c5237967f8fb51
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-01 17:11:52 -0500

    add login ctrl tests

---
+++ src/real-time-trade/login/login-ctrl.spec.js
@@ -0,0 +1,25 @@
+describe('LoginCtrl', function () {
+    var ctrl, createController, $mdDialog;
+
+    beforeEach(function () {
+        module('Login');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope, _$mdDialog_) {
+        createController = function () {
+            ctrl = $controller('LoginCtrl', {
+                $scope: $rootScope.$new()
+            });
+        }
+
+        $mdDialog = _$mdDialog_;
+    }));
+
+    describe('initialization', function () {
+        it('opens the login modal', function () {
+            spyOn($mdDialog, 'show');
+            createController();
+            expect($mdDialog.show).toHaveBeenCalled();
+        });
+    });
+});
