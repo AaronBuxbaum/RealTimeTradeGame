103dc1ec4d48b7b8681da600cdaa044e54679620
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-02 18:35:09 -0500

    Test adding a portfolio test

---
+++ e2e-tests/portfolio.spec.js
@@ -0,0 +1,12 @@
+describe('portfolio interface', function () {
+    var tabs = element(by.tagName('md-tab'));
+
+    beforeEach(function () {
+        browser.get('http://localhost:8000');
+    });
+
+    it('should navigate to the portfolio page', function () {
+        tabs.get(1).click();
+        expect(element(by.cssContainingText('.md-title', 'Looks like there\'s nothing here yet!')).isPresent()).toBe(true);
+    });
+});
