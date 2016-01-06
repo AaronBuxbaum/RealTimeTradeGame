06a4c345a993104e5e3082706c7620a9b759d74f
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-06 18:03:56 -0500

    Add unit tests to historical data controller

--- src/portfolio/historical-stock-data/historical-stock-data-ctrl.spec.js
+++ src/portfolio/historical-stock-data/historical-stock-data-ctrl.spec.js
@@ -1,13 +1,55 @@
 describe('HistoricalStockDataCtrl', function () {
-    var ctrl, createController;
+    var ctrl, createController, HistoricalStockDataService, $q, $rootScope, chartSpy;
 
     beforeEach(function () {
         module('RealTimeTrade.Portfolio');
     });
 
-    beforeEach(inject(function ($controller, $rootScope) {
-        createController = function () {
-            ctrl = $controller('HistoricalStockDataCtrl', { $scope: $rootScope.$new() });
+    beforeEach(inject(function ($controller, _$rootScope_, _$q_, _HistoricalStockDataService_) {
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+        HistoricalStockDataService = _HistoricalStockDataService_;
+        createController = function (symbol) {
+            chartSpy = jasmine.createSpyObj('chart', ['hideLoading', 'reflow', 'addSeries']);
+            ctrl = $controller('HistoricalStockDataCtrl', {}, { symbol: symbol, chart: chartSpy });
         };
     }));
+
+    describe('initialization', function () {
+        it('creates the chart options', function () {
+            createController();
+            expect(_.isObject(ctrl.chartOptions)).toBeTruthy();
+        });
+
+        it('does only calls the historical data service with proper symbol', function () {
+            spyOn(HistoricalStockDataService, 'getHistoricalStockValues').and.returnValue($q.reject());
+
+            createController();
+            createController(3);
+            createController('');
+            createController({ symbol: 'AAPL' });
+            createController('AAAPL');
+            expect(HistoricalStockDataService.getHistoricalStockValues).not.toHaveBeenCalled();
+
+            createController('AAPL');
+            expect(HistoricalStockDataService.getHistoricalStockValues).toHaveBeenCalled();
+        });
+
+        it('sets some chart data on success', function () {
+            spyOn(HistoricalStockDataService, 'getHistoricalStockValues').and.returnValue($q.when(['1']));
+
+            createController('AAPL');
+            $rootScope.$apply();
+            expect(ctrl.chart.addSeries).toHaveBeenCalledWith(({ name: 'AAPL', data: ['1'] }));
+        });
+
+        it('cleans itself up regardless of success or failure', function () {
+            spyOn(HistoricalStockDataService, 'getHistoricalStockValues').and.returnValue($q.reject());
+
+            createController('AAPL');
+            $rootScope.$apply();
+            expect(ctrl.chart.hideLoading).toHaveBeenCalled();
+            expect(ctrl.chart.reflow).toHaveBeenCalled();
+        });
+    });
 });
