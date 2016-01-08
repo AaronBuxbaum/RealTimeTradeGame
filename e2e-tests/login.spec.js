da73738b751f2521364d37070b318d53f97cbf86
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-08 10:53:01 -0500

    add logging out to authentication test

--- e2e-tests/login.spec.js
+++ e2e-tests/login.spec.js
@@ -1,29 +1,34 @@
-describe('log in interface', function () {
-  var dialog = element(by.tagName('md-dialog'));
-  var buttons = element(by.tagName('md-dialog-actions')).all(by.tagName('button'));
+describe('authentication interface', function () {
+    var dialog = element(by.tagName('md-dialog'));
+    var buttons = element(by.tagName('md-dialog-actions')).all(by.tagName('button'));
 
-  beforeEach(function() {
-    browser.get('http://localhost:8000');
-  });
+    beforeEach(function () {
+        browser.get('http://localhost:8000');
+    });
 
-  it('should allow you to log in', function () {
-    var logIn = buttons.first();
+    it('should allow you to authenticate in and out', function () {
+        var logIn = buttons.first();
 
-    //Fail for invalid email
-    element(by.model('ctrl.email')).sendKeys('example');
-    element(by.model('ctrl.password')).sendKeys('examplepassword');
-    logIn.click();
-    expect(dialog.isPresent()).toBe(true);
+        //Fail for invalid email
+        element(by.model('ctrl.email')).sendKeys('example');
+        element(by.model('ctrl.password')).sendKeys('examplepassword');
+        logIn.click();
+        expect(dialog.isPresent()).toBe(true);
 
-    //Fail for non-existant account
-    element(by.model('ctrl.email')).sendKeys('@example.com');
-    logIn.click();
-    expect(dialog.isPresent()).toBe(true);
+        //Fail for non-existant account
+        element(by.model('ctrl.email')).sendKeys('@example.com');
+        logIn.click();
+        expect(dialog.isPresent()).toBe(true);
     
-    //Success
-    element(by.model('ctrl.email')).sendKeys('a@a.com');
-    element(by.model('ctrl.password')).sendKeys('a');
-    logIn.click();
-    expect(dialog.isPresent()).toBe(false);
-  });
+        //Success
+        element(by.model('ctrl.email')).sendKeys('a@a.com');
+        element(by.model('ctrl.password')).sendKeys('a');
+        logIn.click();
+        expect(dialog.isPresent()).toBe(false);
+
+        //Log Out
+        element.all(by.tagName('md-tab-item')).last.click();
+        element(by.buttonText('Log Out')).click();
+        expect(dialog.isPresent()).toBe(true);
+    });
 });
