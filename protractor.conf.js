a48eefcf9f811237e097019cbee3faa4f739337a
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-09 14:58:09 -0500

    Possibly have it wait for highcharts to finish?

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,13 +5,11 @@ exports.config = {
         'browserName': 'chrome',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER,
-        'name': 'Build #{process.env.TRAVIS_BUILD_NUMBER}'
+        'name': 'Build #' + process.env.TRAVIS_BUILD_NUMBER
     },
     framework: 'jasmine',
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
     specs: ['e2e-tests/**/*.spec.js'],
-    onPrepare: function () {
-        browser.params.MockFirebase = require('mockfirebase');
-    }
+    onPrepare: function () { }
 };
