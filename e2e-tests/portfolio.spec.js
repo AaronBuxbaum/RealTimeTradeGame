b9bae53595a87c7bce10b70abc9040b3a2bb9230
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-10 15:24:42 -0500

    Add another portfolio test

--- e2e-tests/portfolio.spec.js
+++ e2e-tests/portfolio.spec.js
@@ -4,18 +4,30 @@ describe('portfolio interface', function () {
         element.all(by.tagName('md-tab-item')).get(1).click();
     });
 
-    describe('initalization', function () {
-        it('should navigate to an empty portfolio page', function () {
-            var nothingHereText = element(by.cssContainingText('.md-title', "Looks like there's nothing here yet!"));
-            expect(nothingHereText.isPresent()).toBeTruthy();
-            var hundredPercent = element(by.binding('ctrl.getMax(0)'));
-            expect(hundredPercent.isPresent()).toBeTruthy();
-            expect(hundredPercent.getText()).toEqual('100%');
-        });
+    it('should navigate to an empty portfolio page', function () {
+        var nothingHereText = element(by.cssContainingText('.md-title', "Looks like there's nothing here yet!"));
+        expect(nothingHereText.isPresent()).toBeTruthy();
+        var hundredPercent = element(by.binding('ctrl.getMax(0)'));
+        expect(hundredPercent.isPresent()).toBeTruthy();
+        expect(hundredPercent.getText()).toEqual('100%');
+    });
 
-        it('should have an add stock button', function () {
-            var addStock = element(by.buttonText('Add Stock'));
-            expect(addStock.isPresent()).toBeTruthy();
-        });
+    it('adds a stock', function () {
+        element(by.buttonText('Add Stock')).click();
+        element(by.tagName('md-autocomplete')).sendKeys('Apple');
+        browser.actions().sendKeys(protractor.Key.ENTER).perform();
+        element(by.buttonText('Confirm')).click();
+
+        var addedStock = element(by.repeater('stock in ctrl.portfolio')).row(0);
+        expect(addedStock.column('stock.name').getText()).toEqual('Apple Inc.');
+        expect(addedStock.column('stock.percentage').getText()).toEqual('0');
+    });
+
+    it('updates the percentage', function () {
+
+    });
+
+    it('removes a stock', function () {
+
     });
 });
