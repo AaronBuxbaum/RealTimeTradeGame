77346a17f882f58bf9ec4319be31f981442f4a15
Aaron Buxbaum <Aaron.Buxbaum@raymondjames.com>
2016-01-14 19:10:49 -0500

    Go directly to browser for test

--- e2e-tests/portfolio.spec.js
+++ e2e-tests/portfolio.spec.js
@@ -1,7 +1,7 @@
 describe('portfolio interface', function () {
     beforeEach(function () {
-        browser.get('http://localhost:8000');
-        element.all(by.tagName('md-tab-item')).get(1).click();
+        browser.get('http://localhost:8000/#/tabs/portfolio');
+        //element.all(by.tagName('md-tab-item')).get(1).click();
     });
 
     it('runs through the basic stock functions', function () {
