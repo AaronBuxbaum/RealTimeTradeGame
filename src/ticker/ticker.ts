7e6061768174aef895a56e7239ad440e56741d36
Aaron Buxbaum <me@aaronbuxbaum.com>
2016-02-16 11:32:38 -0500

    Try changing to component

---
+++ src/ticker/ticker.ts
@@ -0,0 +1,12 @@
+/**
+* @ngdoc component
+* @name RealTimeTrade.Ticker.component:ticker
+* @element ticker
+* @scope
+*
+* @description
+**/
+angular.module('RealTimeTrade.Ticker').component('ticker', {
+  templateUrl: 'ticker/ticker.html',
+  controller: 'TickerCtrl'
+});
