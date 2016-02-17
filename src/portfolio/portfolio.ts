04e61d975a2e4b35985ce462a3d41e24ff1c33fe
Aaron Buxbaum <me@aaronbuxbaum.com>
2016-02-17 00:37:45 -0500

    Portfolio should be a component

---
+++ src/portfolio/portfolio.ts
@@ -0,0 +1,12 @@
+/**
+* @ngdoc directive
+* @name RealTimeTrade.component:portfolio
+* @element portfolio
+* @scope
+*
+* @description
+**/
+angular.module('RealTimeTrade').component('portfolio', {
+	templateUrl: 'portfolio/portfolio.html',
+	controller: 'PortfolioCtrl'
+});
